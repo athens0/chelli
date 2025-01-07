0b07b29817835d9893f2adf3bd312df883dd1353
athens0 <athens1ru@gmail.com>
2025-01-08 01:15:36 +0300

    security changes

--- chelli/settings.py
+++ chelli/settings.py
@@ -22,6 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-27-z1kmoq0#$u4+%=48o4(7b_w+qnn1#5&7h7o)k+*nbv+#isw'
 
+# SSL and HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
