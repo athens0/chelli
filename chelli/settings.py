7520243d875dea1637680f264c3d6eebd178553e
athens0 <athens1ru@gmail.com>
2025-02-07 23:21:12 +0300

    big update

--- chelli/settings.py
+++ chelli/settings.py
@@ -148,7 +148,7 @@ LOGIN_URL = '/login/'
 
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = '127.0.0.1'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'info@chelli.ru'
